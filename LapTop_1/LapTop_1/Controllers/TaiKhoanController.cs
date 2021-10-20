ed2406b74a2cff2510275c0ea45978ed5cc2f0ee
Ducvuive2 <82598608+Ducvuive2@users.noreply.github.com>
2021-10-20 12:55:57 +0700

    Update_
    Update_

---
+++ LapTop_1/LapTop_1/Controllers/TaiKhoanController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using LapTop_1.Models;
+using Volo.Abp.Data;
+
+namespace LapTop_1.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public TaiKhoanController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+  
+        public IActionResult Index()
+        {
+
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            List<TaiKhoan> list = new List<TaiKhoan>();
+            string query = "select * from _CODE_TAIKHOAN";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new TaiKhoan()
+                        {
+                            Tendangnhap = reader["TENDANGNHAP"].ToString(),
+                            Matkhau = reader["MATKHAU"].ToString()
+                        });
+
+                    }
+
+                }
+            }
+            return View(list);
+        }
+    }
+}
---
+++ LapTop_1/LapTop_1/Models/TaiKhoan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LapTop_1.Models
+{
+    public class TaiKhoan
+    {
+        string tendangnhap;
+        string matkhau;
+
+        public string Tendangnhap { get => tendangnhap; set => tendangnhap = value; }
+        public string Matkhau { get => matkhau; set => matkhau = value; }
+
+        public TaiKhoan()
+        {
+        }
+
+        public TaiKhoan(string tendangnhap, string matkhau)
+        {
+            this.tendangnhap = tendangnhap;
+            this.matkhau = matkhau;
+        }
+    }
+}
